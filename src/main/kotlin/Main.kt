301bd75f17765521e70205bfc00a3afcbb6f456d
Victoria Yolkina <vikathebest2030@mail.ru>
2020-10-21 19:05:37 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+package ru.netology
+
+import com.ibm.icu.text.PluralRules
+import sun.text.resources.PluralRules
+import java.util.*
+
+fun main() {
+    val locale = Locale("RU")
+    val plurals = PluralRules.forLocale(locale)
+    val likes = 1_000
+
+    val numberOfLikes = getLikes(likes, plurals)
+    println("Понравилось $numberOfLikes")
+}
+
+private fun getLikes(likes: Int, plurals: PluralRules): String {
+    if (likes == 0) {
+        return "нет лайков"
+    }
+
+    val plural = plurals.select(likes.toDouble())
+    return when (plural) {
+        PluralRules.KEYWORD_ONE -> "%d человеку"
+        PluralRules.KEYWORD_FEW -> "%d людям"
+        else -> "%d людям"
+    }.format(likes)
+}
